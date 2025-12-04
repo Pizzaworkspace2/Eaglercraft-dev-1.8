16bd5a65cca962dae806304473d4534ef7e82b47
Marco <5439724524@student.cms.k12.nc.us>
2025-12-04 09:11:55 -0500

    Update RenderItem.java

--- src/game/java/net/minecraft/client/renderer/entity/RenderItem.java
+++ src/game/java/net/minecraft/client/renderer/entity/RenderItem.java
@@ -1106,6 +1106,7 @@ public class RenderItem implements IResourceManagerReloadListener {
 		this.registerBlock(Blocks.red_mushroom_block, BlockHugeMushroom.EnumType.ALL_INSIDE.getMetadata(),
 				"red_mushroom_block");
 		this.registerBlock(Blocks.dragon_egg, "dragon_egg");
+		this.registerBlock(Blocks.loom, "loom");
 	}
 
 	public void onResourceManagerReload(IResourceManager var1) {
@@ -1113,3 +1114,4 @@ public class RenderItem implements IResourceManagerReloadListener {
 	}
 
 }
+
