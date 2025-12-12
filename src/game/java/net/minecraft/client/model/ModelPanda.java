14e512fac1caa57fa2ecfa5cb30b109409a4f73c
Marco <5439724524@student.cms.k12.nc.us>
2025-12-12 08:19:20 -0500

    Update ModelPanda.java

--- src/game/java/net/minecraft/client/model/ModelPanda.java
+++ src/game/java/net/minecraft/client/model/ModelPanda.java
@@ -1,116 +1,75 @@
 package net.minecraft.client.model;
 
-import java.util.Set;
-import net.minecraft.client.model.geom.ModelPart;
-import net.minecraft.client.model.geom.PartPose;
-import net.minecraft.client.model.geom.builders.CubeListBuilder;
-import net.minecraft.client.model.geom.builders.LayerDefinition;
-import net.minecraft.client.model.geom.builders.MeshDefinition;
-import net.minecraft.client.model.geom.builders.MeshTransformer;
-import net.minecraft.client.model.geom.builders.PartDefinition;
-import net.minecraft.client.renderer.entity.state.PandaRenderState;
-import net.minecraft.util.Mth;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraft.entity.Entity;
+import net.minecraft.util.MathHelper; // 1.8 equivalent of Mth
 
-@OnlyIn(Dist.CLIENT)
-public class PandaModel extends QuadrupedModel<PandaRenderState> {
-    public static final MeshTransformer BABY_TRANSFORMER = new BabyModelTransform(true, 23.0F, 4.8F, 2.7F, 3.0F, 49.0F, Set.of("head"));
+//used ai maybe i'll become better at writing code for mobs.
+public class PandaModel extends ModelBase { 
+    public ModelRenderer head;
+    public ModelRenderer body;
+    public ModelRenderer rightHindLeg;
+    public ModelRenderer leftHindLeg;
+    public ModelRenderer rightFrontLeg;
+    public ModelRenderer leftFrontLeg;
 
-    public PandaModel(ModelPart p_170771_) {
-        super(p_170771_);
+    public PandaModel() {
+        this.textureWidth = 64;
+        this.textureHeight = 64;
+
+        // Head setup
+        this.head = new ModelRenderer(this, 0, 6);
+        this.head.addBox(-6.5F, -5.0F, -4.0F, 13, 10, 9);
+        this.head.setRotationPoint(0.0F, 11.5F, -17.0F);
+        this.head.setTextureOffset(45, 16).addBox("nose", -3.5F, 0.0F, -6.0F, 7, 5, 2);
+        this.head.setTextureOffset(52, 25).addBox("left_ear", 3.5F, -8.0F, -1.0F, 5, 4, 1);
+        this.head.setTextureOffset(52, 25).addBox("right_ear", -8.5F, -8.0F, -1.0F, 5, 4, 1);
+        
+        // Body setup
+        this.body = new ModelRenderer(this, 0, 25);
+        this.body.addBox(-9.5F, -13.0F, -6.5F, 19, 26, 13);
+        this.body.setRotationPoint(0.0F, 10.0F, 0.0F);
+        this.body.rotateAngleX = (float) (Math.PI / 2);
+
+        // Legs setup (adjust points as needed for 1.8 coordinates)
+        // In 1.8, leg models usually have the rotation point at the connection to the body/ground
+        this.rightHindLeg = new ModelRenderer(this, 40, 0);
+        this.rightHindLeg.addBox(-3.0F, 0.0F, -3.0F, 6, 9, 6);
+        this.rightHindLeg.setRotationPoint(-5.5F, 15.0F, 9.0F);
+
+        this.leftHindLeg = new ModelRenderer(this, 40, 0);
+        this.leftHindLeg.addBox(-3.0F, 0.0F, -3.0F, 6, 9, 6);
+        this.leftHindLeg.setRotationPoint(5.5F, 15.0F, 9.0F);
+
+        this.rightFrontLeg = new ModelRenderer(this, 40, 0);
+        this.rightFrontLeg.addBox(-3.0F, 0.0F, -3.0F, 6, 9, 6);
+        this.rightFrontLeg.setRotationPoint(-5.5F, 15.0F, -9.0F);
+
+        this.leftFrontLeg = new ModelRenderer(this, 40, 0);
+        this.leftFrontLeg.addBox(-3.0F, 0.0F, -3.0F, 6, 9, 6);
+        this.leftFrontLeg.setRotationPoint(5.5F, 15.0F, -9.0F);
     }
 
-    public static LayerDefinition createBodyLayer() {
-        MeshDefinition meshdefinition = new MeshDefinition();
-        PartDefinition partdefinition = meshdefinition.getRoot();
-        partdefinition.addOrReplaceChild(
-            "head",
-            CubeListBuilder.create()
-                .texOffs(0, 6)
-                .addBox(-6.5F, -5.0F, -4.0F, 13.0F, 10.0F, 9.0F)
-                .texOffs(45, 16)
-                .addBox("nose", -3.5F, 0.0F, -6.0F, 7.0F, 5.0F, 2.0F)
-                .texOffs(52, 25)
-                .addBox("left_ear", 3.5F, -8.0F, -1.0F, 5.0F, 4.0F, 1.0F)
-                .texOffs(52, 25)
-                .addBox("right_ear", -8.5F, -8.0F, -1.0F, 5.0F, 4.0F, 1.0F),
-            PartPose.offset(0.0F, 11.5F, -17.0F)
-        );
-        partdefinition.addOrReplaceChild(
-            "body",
-            CubeListBuilder.create().texOffs(0, 25).addBox(-9.5F, -13.0F, -6.5F, 19.0F, 26.0F, 13.0F),
-            PartPose.offsetAndRotation(0.0F, 10.0F, 0.0F, (float) (Math.PI / 2), 0.0F, 0.0F)
-        );
-        int i = 9;
-        int j = 6;
-        CubeListBuilder cubelistbuilder = CubeListBuilder.create().texOffs(40, 0).addBox(-3.0F, 0.0F, -3.0F, 6.0F, 9.0F, 6.0F);
-        partdefinition.addOrReplaceChild("right_hind_leg", cubelistbuilder, PartPose.offset(-5.5F, 15.0F, 9.0F));
-        partdefinition.addOrReplaceChild("left_hind_leg", cubelistbuilder, PartPose.offset(5.5F, 15.0F, 9.0F));
-        partdefinition.addOrReplaceChild("right_front_leg", cubelistbuilder, PartPose.offset(-5.5F, 15.0F, -9.0F));
-        partdefinition.addOrReplaceChild("left_front_leg", cubelistbuilder, PartPose.offset(5.5F, 15.0F, -9.0F));
-        return LayerDefinition.create(meshdefinition, 64, 64);
+    @Override
+    public void setRotationAngles(float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch, float scaleFactor, Entity entityIn) {
+        // Reset rotations for standard movement
+        this.head.rotateAngleZ = 0.0F;
+        this.rightFrontLeg.rotateAngleX = MathHelper.cos(limbSwing * 0.6662F) * 1.4F * limbSwingAmount;
+        this.leftFrontLeg.rotateAngleX = MathHelper.cos(limbSwing * 0.6662F + (float)Math.PI) * 1.4F * limbSwingAmount;
+        this.rightHindLeg.rotateAngleX = MathHelper.cos(limbSwing * 0.6662F + (float)Math.PI) * 1.4F * limbSwingAmount;
+        this.leftHindLeg.rotateAngleX = MathHelper.cos(limbSwing * 0.6662F) * 1.4F * limbSwingAmount;
+
     }
 
-    public void setupAnim(PandaRenderState p_368382_) {
-        super.setupAnim(p_368382_);
-        if (p_368382_.isUnhappy) {
-            this.head.yRot = 0.35F * Mth.sin(0.6F * p_368382_.ageInTicks);
-            this.head.zRot = 0.35F * Mth.sin(0.6F * p_368382_.ageInTicks);
-            this.rightFrontLeg.xRot = -0.75F * Mth.sin(0.3F * p_368382_.ageInTicks);
-            this.leftFrontLeg.xRot = 0.75F * Mth.sin(0.3F * p_368382_.ageInTicks);
-        } else {
-            this.head.zRot = 0.0F;
-        }
-
-        if (p_368382_.isSneezing) {
-            if (p_368382_.sneezeTime < 15) {
-                this.head.xRot = (float) (-Math.PI / 4) * (float)p_368382_.sneezeTime / 14.0F;
-            } else if (p_368382_.sneezeTime < 20) {
-                float f = (float)((p_368382_.sneezeTime - 15) / 5);
-                this.head.xRot = (float) (-Math.PI / 4) + (float) (Math.PI / 4) * f;
-            }
-        }
-
-        if (p_368382_.sitAmount > 0.0F) {
-            this.body.xRot = Mth.rotLerpRad(p_368382_.sitAmount, this.body.xRot, 1.7407963F);
-            this.head.xRot = Mth.rotLerpRad(p_368382_.sitAmount, this.head.xRot, (float) (Math.PI / 2));
-            this.rightFrontLeg.zRot = -0.27079642F;
-            this.leftFrontLeg.zRot = 0.27079642F;
-            this.rightHindLeg.zRot = 0.5707964F;
-            this.leftHindLeg.zRot = -0.5707964F;
-            if (p_368382_.isEating) {
-                this.head.xRot = (float) (Math.PI / 2) + 0.2F * Mth.sin(p_368382_.ageInTicks * 0.6F);
-                this.rightFrontLeg.xRot = -0.4F - 0.2F * Mth.sin(p_368382_.ageInTicks * 0.6F);
-                this.leftFrontLeg.xRot = -0.4F - 0.2F * Mth.sin(p_368382_.ageInTicks * 0.6F);
-            }
-
-            if (p_368382_.isScared) {
-                this.head.xRot = 2.1707964F;
-                this.rightFrontLeg.xRot = -0.9F;
-                this.leftFrontLeg.xRot = -0.9F;
-            }
-        } else {
-            this.rightHindLeg.zRot = 0.0F;
-            this.leftHindLeg.zRot = 0.0F;
-            this.rightFrontLeg.zRot = 0.0F;
-            this.leftFrontLeg.zRot = 0.0F;
-        }
-
-        if (p_368382_.lieOnBackAmount > 0.0F) {
-            this.rightHindLeg.xRot = -0.6F * Mth.sin(p_368382_.ageInTicks * 0.15F);
-            this.leftHindLeg.xRot = 0.6F * Mth.sin(p_368382_.ageInTicks * 0.15F);
-            this.rightFrontLeg.xRot = 0.3F * Mth.sin(p_368382_.ageInTicks * 0.25F);
-            this.leftFrontLeg.xRot = -0.3F * Mth.sin(p_368382_.ageInTicks * 0.25F);
-            this.head.xRot = Mth.rotLerpRad(p_368382_.lieOnBackAmount, this.head.xRot, (float) (Math.PI / 2));
-        }
-
-        if (p_368382_.rollAmount > 0.0F) {
-            this.head.xRot = Mth.rotLerpRad(p_368382_.rollAmount, this.head.xRot, 2.0561945F);
-            this.rightHindLeg.xRot = -0.5F * Mth.sin(p_368382_.ageInTicks * 0.5F);
-            this.leftHindLeg.xRot = 0.5F * Mth.sin(p_368382_.ageInTicks * 0.5F);
-            this.rightFrontLeg.xRot = 0.5F * Mth.sin(p_368382_.ageInTicks * 0.5F);
-            this.leftFrontLeg.xRot = -0.5F * Mth.sin(p_368382_.ageInTicks * 0.5F);
-        }
+    @Override
+    // The 1.8 method to render the model
+    public void render(Entity entityIn, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch, float scale) {
+        this.setRotationAngles(limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch, scale, entityIn);
+        this.head.render(scale);
+        this.body.render(scale);
+        this.rightHindLeg.render(scale);
+        this.leftHindLeg.render(scale);
+        this.rightFrontLeg.render(scale);
+        this.leftFrontLeg.render(scale);
     }
 }
+
